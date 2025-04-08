4b54f9c6f112d7ff2fa922e7294f0dafbb5798a4
root <root@tux-IaC.labo.lan>
2025-04-08 14:33:17 +0200

    Add role B & C

---
+++ add_role_C.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ansible-playbook -i inventory/infra-C/ playbooks/configure_M2L-WSRV01.yml
+
+ansible-playbook -i inventory/infra-C/ playbooks/configure_M2L-WSRV02.yml
+
+ansible-playbook -i inventory/infra-C/ playbooks/configure_M2L-LSRV03.yml
+
+ansible-playbook -i inventory/infra-C/ playbooks/configure_M2L-LSRV05.yml
+
